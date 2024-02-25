a375486f14356b4de76556fa1268782f321b5bf1
juni <juniejere6654@gmail.com>
2024-02-25 09:39:59 -0600

    Updated barcode scanner to print the string

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 import base64
-from PIL import Image
-from pyzbar.pyzbar import decode, ZBarSymbol
+# from PIL import Image
+# from pyzbar.pyzbar import decode, ZBarSymbol
 
 
 app = Flask(__name__,static_url_path='/static', static_folder='./static')
 
-def find_barcode():
-    print("IM working")
+# def find_barcode():
+#     print("IM working")
     
 
 # first route when user goes to website.
@@ -15,20 +15,34 @@ def find_barcode():
 def index():
     return render_template('scanner.html'), 200
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    # Get the image data URL from the request
-    image_data_url = request.json['imageDataURL']
+# @app.route('/upload', methods=['POST'])
+# def upload():
+#     # Get the image data URL from the request
+#     image_data_url = request.json['imageDataURL']
     
-    # Process the image data URL as needed (e.g., save it to a file, analyze it, etc.)
-    # Example: Save the image to a file
-    with open('captured_photo.jpg', 'wb') as f:
+#     # Process the image data URL as needed (e.g., save it to a file, analyze it, etc.)
+#     # Example: Save the image to a file
+#     with open('captured_photo.jpg', 'wb') as f:
+#        # f.write(image_data_url.split(',')[1].decode('base64'))
+#         f.write(base64.b64decode(image_data_url.split(',')[1]))
+
+#     # find_barcode()
+#     return jsonify({'message': 'Image uploaded successfully' + extraStr})
+
+@app.route('/upload_code', methods=['POST'])
+def upload_code():
+    # Get the image data URL from the request
+    code = request.json['code']
+    print(code)
+    # print(code.encode('utf-8'))
+    # Save the barcode data to a file
+    with open('barcode_txt', 'wb') as f:
        # f.write(image_data_url.split(',')[1].decode('base64'))
-        f.write(base64.b64decode(image_data_url.split(',')[1]))
+        # f.write(code)
+        f.write(code.encode('utf-8'))
 
-    find_barcode()
-    return jsonify({'message': 'Image uploaded successfully' + extraStr})
-
+    # find_barcode()
+    return jsonify({'message': 'Code uploaded successfully'})
 
 
 if __name__ == '__main__':
--- static/scanner1.js
+++ static/scanner1.js
@@ -1,7 +1,43 @@
 // import Quagga from "../dist/quagga.min.js"; // Adjust the path as needed
 
+// const { Quagga } = require("../quagga");
+
+//return array sorted by the most common value found first
+function order_by_occurrence(arr) {
+  var counts = {};
+  arr.forEach(function (value) {
+    if (!counts[value]) {
+      counts[value] = 0;
+    }
+    counts[value]++;
+  });
+
+  return Object.keys(counts).sort(function (curKey, nextKey) {
+    return counts[curKey] < counts[nextKey];
+  });
+}
 const loadQuagga = () => {
   if ("mediaDevices" in navigator && navigator.mediaDevices.getUserMedia) {
+    var last_result = []; //create array of detected images to improve scanning functionality
+
+    if (Quagga.initialized == undefined) {
+      //makes sure Quagga.onDetected only ran once
+      //detect barcodes
+      Quagga.onDetected(function (result) {
+        //once a barcode is detected, save it to last_code and stop video
+        var last_code = result.codeResult.code;
+        last_result.push(last_code);
+        if (last_result.length > 20) {
+          //capture 20 images
+          code = order_by_occurrence(last_result)[0]; //get the image that appears the most often **most accurate**
+          last_result = [];
+          Quagga.stop();
+          sendCodeToFlask(code);
+          /*****SEND DATA TO PYTHON BACKEND */
+        }
+      });
+    }
+
     Quagga.init(
       {
         inputStream: {
@@ -9,11 +45,19 @@ const loadQuagga = () => {
           type: "LiveStream",
           numOfWorkers: navigator.hardwareConcurrency,
           target: document.querySelector("#barcode-scanner"),
-          constraints: {
-            width: 400,
-            height: 400,
-            facingMode: "environment",
-          },
+          //   constraints: {
+          //     width: 400, //{
+          //     //   min: 640,
+          //     //   ideal: 1280,
+          //     //   max: 2560,
+          //     //},
+          //     height: 400, //{
+          //     //   min: 360,
+          //     //   ideal: 720,
+          //     //   max: 1440,
+          //     //},
+          //     facingMode: "environment",
+          //   },
         },
         decoder: {
           readers: [
@@ -32,6 +76,7 @@ const loadQuagga = () => {
           console.error(err);
           return;
         }
+        Quagga.initialized = true; //user defined variable so that onDetected only runs once
         console.log("Initialization finished. Ready to start");
         Quagga.start();
       }
@@ -40,3 +85,24 @@ const loadQuagga = () => {
 };
 
 document.addEventListener("DOMContentLoaded", loadQuagga);
+
+function sendCodeToFlask(code) {
+  // Send a POST request to the Flask backend with the image data
+  fetch("/upload_code", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ code: code }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("code uploaded successfully");
+      } else {
+        console.error("Error uploading code1:", response.statusText);
+      }
+    })
+    .catch((error) => {
+      console.error("Error uploading code2:", error);
+    });
+}
