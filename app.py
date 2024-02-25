1c3c1846a452522145d038bafd25ee6f0827d90c
Emily <emedlin08@gmail.com>
2024-02-24 18:57:43 -0600

    added login functionality

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from utils.database import check_credentials
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
-
+app.secret_key = os.getenv('SECRET_KEY')
 
 dinosaurs_data = {
   "buddy 1",
@@ -16,6 +20,21 @@ dinosaurs_data = {
 @app.route("/")
 def index():
     return render_template('login-screen.html'), 200
+
+@app.route("/login", methods=['POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+
+        # sends the form answers to be verified. If successful, redirect user to the home page
+        if check_credentials(email=email):
+            session['email'] = request.form['email']
+            return redirect(url_for('home'))
+    return redirect("/")
+    
+@app.route("/home", endpoint='home')
+def home_screen():
+    return "<h1> hello world </h1>"
 
 @app.route("/inventory")
 def inventory_screen():
--- utils/database.py
+++ utils/database.py
@@ -1,3 +1,6 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,3 +12,35 @@ dbname = os.getenv('DATABASE')
 
 db_connection_string = f"mysql+pymysql://{user}:{password}@{host}/{dbname}?charset=utf8mb4"
 
+engine = create_engine(
+    db_connection_string,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    }
+)
+
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = 'Users'
+    user_id = Column(Integer, primary_key=True)
+    user_email = Column(String)
+    user_name = Column(String)
+
+# function to check whether account exists or not
+def check_credentials(email):
+    # new database connection
+    session = Session()
+    # query on User table, and filtering the results by email.
+    user = session.query(Users).filter_by(user_email=email).first() 
+
+    # return the status of whether or not the user exists
+    if user:
+        session.close()
+        return True
+    else:
+        session.close()
+        return False
