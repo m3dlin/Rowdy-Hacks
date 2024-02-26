268ee83f7bcec103bf24957b3cbd8e930615c93c
Emily <110800525+m3dlin@users.noreply.github.com>
2024-02-26 10:25:36 -0600

    rowdy hacks final project (#1)

--- app.py
+++ app.py
@@ -1,11 +1,129 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from utils.database import check_credentials, get_dinosaurs_ids, get_dinosaurs_info, get_dinosaur_info, get_dinosaur
+import base64
+import os
+from dotenv import load_dotenv
+import random
+load_dotenv()
 
 app = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY')
+
+# Define probabilities for each number
+probabilities = {
+    1: 0.6,  # 60% chance
+    2: 0.25,  # 25% chance
+    3: 0.1,   # 10% chance
+    4: 0.05   # 5% chance
+}
+
 
 # first route when user goes to website.
 @app.route("/")
 def index():
-    return render_template('index.html'), 200
+    return render_template('login-screen.html'), 200
+
+@app.route("/login", methods=['POST'], endpoint='login')
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+
+        # sends the form answers to be verified. If successful, redirect user to the home page
+        if check_credentials(email=email):
+            session['user'] = request.form['email']
+            return redirect(url_for('home'))
+    return redirect("/")
+    
+@app.route("/home", endpoint='home')
+def home_screen():
+    return render_template('home.html'), 200
+
+
+@app.route("/dinopedia")
+def dinopedia_screen():
+    return render_template('dinopedia.html'), 200
+
+
+@app.route("/scan")
+def scan_screen():
+    return render_template('scanner.html'), 200
+
+
+@app.route("/inventory")
+def inventory_screen():
+    if 'user' in session:
+        user = session['user']
+        dinosaur_ids = get_dinosaurs_ids(user)
+        dinosaur_data = get_dinosaurs_info(dinosaur_ids)
+        
+        # Convert binary image data to Base64-encoded strings
+        dinosaurs = []
+        for dinosaur_name, dinosaur_image in dinosaur_data:
+            base64_image = base64.b64encode(dinosaur_image).decode('utf-8')
+            dinosaurs.append({'name': dinosaur_name, 'image': base64_image})
+
+        return render_template('inventory.html', dinosaurs=dinosaurs), 200
+    return redirect(url_for('login'))
+
+@app.route("/<dino_name>")
+def dino_page(dino_name):
+        if 'user' in session:
+            user = session['user']
+            dinosaur_info = get_dinosaur_info(dino_name)
+            dinosaur_image = dinosaur_info[1]
+
+            
+            base64_image = base64.b64encode(dinosaur_info[1]).decode('utf-8')
+            dino = {'name': dinosaur_image, 'image': base64_image}
+
+            return render_template('dino-entry.html', dinosaur=dino), 200
+
+
+
+
+@app.route('/upload_code', methods=['POST'])
+def upload_code():
+    if request.method == 'POST':
+    # Get the image data URL from the request
+        code = request.json['code']
+
+        return redirect(url_for('process_code'))
+    # 4101450004474
+    # # print(code.encode('utf-8'))
+    # # Save the barcode data to a file
+    # with open('barcode_txt', 'wb') as f:
+    #    # f.write(image_data_url.split(',')[1].decode('base64'))
+    #     # f.write(code)
+    #     f.write(code.encode('utf-8'))
+
+    # find_barcode()
+    # return jsonify({'message': 'Code uploaded successfully'})
+
+@app.route("/process_code", endpoint='process_code', methods=['GET'])
+def process_code():
+    # Your code to process the uploaded code
+    # ...
+
+    # After processing, redirect to the capture route
+    return redirect(url_for('capture'))
+
+
+
+
+@app.route("/capture", endpoint='capture', methods=['GET'])
+def  dino_capture_screen():
+    # Generate a random number based on probabilities
+    dinoID = random.choices(list(probabilities.keys()), list(probabilities.values()))[0]
+    
+
+    dinosaur_info = get_dinosaur(dinoID)
+    dinosaur_image = dinosaur_info[1]
+
+            
+    base64_image = base64.b64encode(dinosaur_info[1]).decode('utf-8')
+    dino = {'name': dinosaur_image, 'image': base64_image}
+    return render_template('dino-capture.html', dinosaur=dino), 200
 
 
 
---
+++ utils/database.py
@@ -0,0 +1,123 @@
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
+# Reads credentials from the config file
+user = os.getenv('DATABASE_USERNAME')
+password = os.getenv('DATABASE_PASSWORD')
+host = os.getenv('DATABASE_HOST')
+dbname = os.getenv('DATABASE')
+
+db_connection_string = f"mysql+pymysql://{user}:{password}@{host}/{dbname}?charset=utf8mb4"
+
+engine = create_engine(
+    db_connection_string,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem"
+        }
+    }
+)
+
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = 'Users'
+    user_id = Column(Integer, primary_key=True)
+    user_email = Column(String)
+    user_name = Column(String)
+
+class Users_has_Dinosaurs(Base):
+    __tablename__ = 'Users_has_Dinosaurs'
+    Users_user_id = Column(Integer, primary_key=True)
+    Dinosaurs_dino_id = Column(Integer,primary_key=True)
+
+
+# function to check whether account exists or not
+def check_credentials(email):
+    # new database connection
+    session = Session()
+    # query on User table, and filtering the results by email.
+    user = session.query(Users).filter_by(user_email=email).first() 
+
+    # return the status of whether or not the user exists
+    if user:
+        session.close()
+        return True
+    else:
+        session.close()
+        return False
+    
+# function to retrieve dinosaurs that the user has in their inventory
+def get_dinosaurs_ids(email):
+    session = Session()
+    user = session.query(Users).filter_by(user_email=email).first()
+
+    # if the user exists
+    if user:
+        conn = engine.connect()
+        # execute SQL
+        query = text('SELECT Dinosaurs_dino_id FROM Users_has_Dinosaurs WHERE Users_user_id = :user_id')
+        result = conn.execute(query,{'user_id': user.user_id })
+
+
+        # Fetch all rows from the result
+        dinosaurs = result.fetchall()
+        conn.close()
+
+        # take dinosaur IDs from the result
+        dinosaur_ids = [row[0] for row in dinosaurs]
+
+        return dinosaur_ids
+    else:
+        return []  # Return an empty list if user is not found or email is invalid
+
+def get_dinosaurs_info(ids):
+    with engine.connect() as conn:
+        dinosaurs = []
+        query = text("SELECT dino_breed, dino_image FROM Dinosaurs WHERE dino_id = :dino_id")
+        for dino_id in ids:
+            result = conn.execute(query, {'dino_id': dino_id})
+            dinosaur_info = result.fetchone()
+            if dinosaur_info:
+                dinosaurs.append(dinosaur_info)
+
+        return dinosaurs
+    
+
+def get_dinosaur_info(name):
+        with engine.connect() as conn:
+            query = text("SELECT dino_breed, dino_image FROM Dinosaurs WHERE dino_breed = :dino_breed")
+        
+            result = conn.execute(query, {'dino_breed': name})
+            dinosaur_info = result.fetchone()
+            if dinosaur_info:
+                return dinosaur_info
+    
+        return None
+
+
+def image_to_blob(image_path):
+    with open(image_path, 'rb') as file:
+        blob_data = file.read()
+    return blob_data
+
+def insert_blob(dino_id, blob_data):
+    with engine.connect() as conn:
+        query = text("UPDATE Dinosaurs SET dino_image = :blob_data WHERE dino_id = :dino_id")
+        conn.execute(query, {'blob_data': blob_data, 'dino_id': dino_id})
+
+
+def get_dinosaur(id):
+        with engine.connect() as conn:
+            query = text("SELECT dino_breed, dino_image FROM Dinosaurs WHERE dino_id = :dino_id")
+        
+            result = conn.execute(query, {'dino_id': id})
+            dinosaur_info = result.fetchone()
+            if dinosaur_info:
+                return dinosaur_info
+    
+        return None
